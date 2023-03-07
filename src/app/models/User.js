0ca5bb5b25f78297812acc8c165472269133dec9
Trần Trọng Nhân <nhatduy1284t@gmail.com>
2023-03-07 11:22:58 +0700

    m

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -4,11 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    description: { type: String },
-    image: { type: String },
-    videoId: { type: String, required: true },
-    level: { type: String },
-    slug: { type: String, slug: "name", unique: true },
+    password: { type: String },
   },
   { collection: "user" }
 );
