8bca9a8c3766adc851ea357d663af8e9701af6e5
kiet-git <89214459+kiet-git@users.noreply.github.com>
2023-03-07 11:12:53 +0700

    handlebars_added

--- src/index.js
+++ src/index.js
@@ -1,46 +1,22 @@
-// import express from "express";
-// import { engine } from "express-handlebars";
-const express = require("express");
-const { engine } = require("express-handlebars");
+import express from 'express';
+import { engine } from 'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/app/views/')
+app.use(express.static(path.join(__dirname,'/app/views/')))
 
-const dogs = [
-  { name: "Jimmy", age: 12 },
-  { name: "Johnny", age: 32 },
-];
-
-app.get("/", (req, res) => {
-  console.log("da co nguoi request");
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.json(dogs);
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get("/dogs/:id", (req, res) => {
-  dog = dogs[parseInt(req.params.id) - 1];
-  res.send(dog);
-});
-
-app.listen(3000);
+app.listen(port, () => console.log(`Example app listening at ${port}`));
 
