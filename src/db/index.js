bcb720da2ea66d74efb44786228ee788a7f58e3b
Trần Trọng Nhân <nhatduy1284t@gmail.com>
2023-03-07 11:17:58 +0700

    example User model

--- src/db/index.js
+++ src/db/index.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/f8_education_dev', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        });
-        console.log('Connect successfully!!!');
+        await mongoose.connect('mongodb://localhost:27017/classroommanagement');
+        console.log('Connect successfully!!!'); 
     } catch (error) {
         console.log('Connect failure!!!');
     }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-// import express from "express";
-// import { engine } from "express-handlebars";
 const express = require("express");
-const { engine } = require("express-handlebars");
-
 const app = express();
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
+const db = require("./db");
+
+//Connect to db
+db.connect();
 
 const dogs = [
   { name: "Jimmy", age: 12 },
@@ -18,29 +15,27 @@ app.get("/", (req, res) => {
   console.log("da co nguoi request");
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-
   // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.json(dogs);
 });
 
-app.get("/dogs/:id", (req, res) => {
-  dog = dogs[parseInt(req.params.id) - 1];
-  res.send(dog);
+app.get("/dogs", async (req, res) => {
+  const testUser = require("./app/models/User");
+  const result = await testUser.find({});
+  console.log(result); // 'hello'
+  res.json(result);
 });
 
 app.listen(3000);
-
